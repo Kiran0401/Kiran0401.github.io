cc8a40cadeeed0cf89e272fba1741c5ac125792f
Kiran0401 <100419707+Kiran0401@users.noreply.github.com>
2022-05-31 17:19:33 +0800

    Add files via upload

---
+++ puzzle/puzzle.js
@@ -0,0 +1,53 @@
+function sayHello(){
+  var response = prompt("What's your name?");
+  alert("Hello " + response + ", welcome to Online Latern Show!");
+}
+sayHello()
+
+function lattern1(){
+  var answer1 = prompt("客中觅得意中人（打一字）")
+  if (answer1==="复")
+  alert("回答正确，太棒了！")
+  else
+  alert("啊哦（悄悄提示：“客”中，“意”中，还有“人”，组字）")
+}
+
+function lattern2(){
+  var answer2 = prompt("双木非林，田下有心（打二字词语）")
+  if (answer2==="相思")
+  alert("回答正确，太棒了！")
+  else
+  alert("悄悄提示：不同的mù组字，“田”和“心”组字）")
+}
+
+function lattern3(){
+  var answer3 = prompt("春末秋初花芬芳（打一字）")
+  if (answer3==="香")
+  alert("回答正确，太棒了！")
+  else
+  alert("啊哦（悄悄提示：“春”末，“秋”中初，“花芬芳”可以用这个字概括）")
+}
+
+function lattern4(){
+  var answer4 = prompt("逢十进一下去三（打一字）")
+  if (answer4==="垒")
+  alert("回答正确，太棒了！")
+  else
+  alert("啊哦（悄悄提示：“十”和“一”组字后，加上三个“去”的下面，组字）")
+}
+
+function lattern5(){
+  var answer5 = prompt("夕望长安自伤心（打一成语）")
+  if (answer5==="朝秦暮楚")
+  alert("回答正确，太棒了！")
+  else
+  alert("啊哦（悄悄提示：长安曾属于“秦”地，“伤心”用一个字来表示）")
+}
+
+function lattern6(){
+  var answer6 = prompt("招贤纳士平原君（打一成语）")
+  if (answer6==="引人入胜")
+  alert("回答正确，太棒了！")
+  else
+  alert("啊哦（悄悄提示：平原君是赵胜）")
+}
